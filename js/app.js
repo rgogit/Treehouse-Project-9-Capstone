e2484c44e8fca900ee4570800ec9def54c665504
rgogit <79509368+rgogit@users.noreply.github.com>
2021-08-07 22:21:12 -0700

    final

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// change element colors on hover
+
+// header
+document.getElementById("head").addEventListener("mouseover", function() {
+    document.getElementById("head").style.backgroundColor = "#62bbbb";
+});    
+document.getElementById("head").addEventListener("mouseout", function() {
+    document.getElementById("head").style.backgroundColor = "#008080";
+});
+
+//about section
+document.getElementById("about").addEventListener("mouseover", function() {
+    document.getElementById("about").style.backgroundColor = "#ffffff";
+});    
+document.getElementById("about").addEventListener("mouseout", function() {
+    document.getElementById("about").style.backgroundColor = "#faf3f3";
+});
+
+//skills section
+document.getElementById("skills").addEventListener("mouseover", function() {
+    document.getElementById("skills").style.backgroundColor = "#ffffff";
+});    
+document.getElementById("skills").addEventListener("mouseout", function() {
+    document.getElementById("skills").style.backgroundColor = "#faf3f3";
+});
+
+//projects section
+document.getElementById("projects").addEventListener("mouseover", function() {
+    document.getElementById("projects").style.backgroundColor = "#ffffff";
+});    
+document.getElementById("projects").addEventListener("mouseout", function() {
+    document.getElementById("projects").style.backgroundColor = "#faf3f3";
+});
+
+//contact section
+document.getElementById("contact").addEventListener("mouseover", function() {
+    document.getElementById("contact").style.backgroundColor = "#ffffff";
+});    
+document.getElementById("contact").addEventListener("mouseout", function() {
+    document.getElementById("contact").style.backgroundColor = "#faf3f3";
+});
+
+//foooter
+document.getElementById("foot").addEventListener("mouseover", function() {
+    document.getElementById("foot").style.backgroundColor = "#62bbbb";
+});   
+document.getElementById("foot").addEventListener("mouseout", function() {
+    document.getElementById("foot").style.backgroundColor = "#008080";
+});
